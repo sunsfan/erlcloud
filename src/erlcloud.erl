d1ec2361595f0d0928f1e3a1a3d641d6e86a43f7
sunteng <sunteng@meituan.com>
2018-09-05 19:52:58 +0800

    添加分段上传方法

--- src/erlcloud.erl
+++ src/erlcloud.erl
@@ -3,11 +3,12 @@
 
 
 -include("erlcloud_aws.hrl").
+
 -define(APP, erlcloud).
+
 
 start() ->
     application:load(?APP),
     {ok, Apps} = application:get_key(?APP, applications),
     [application:start(App) || App <- Apps],
     application:start(?APP).
-
